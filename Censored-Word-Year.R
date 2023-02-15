f120695c03dc508ecebd5f269cb847a58e82ad2a
hyoungtakdo <hdo3@uw.edu>
2023-02-15 12:45:25 -0800

    line graph with title

--- Censored-Word-Year.R
+++ Censored-Word-Year.R
@@ -2,4 +2,12 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
-kidz_data <- read.csv("")
+kidz_data <- read.csv("https://raw.githubusercontent.com/the-pudding/data/master/kidz-bop/KB_censored-lyrics.csv")
+
+sum_censored_words <- kidz_data %>% group_by(year) %>% 
+                      summarise(count = sum(count))
+
+ggplot(data = sum_censored_words) + 
+  geom_line(mapping = aes(x = year, y = count)) + ggtitle("Censorship By Year")
+
+
